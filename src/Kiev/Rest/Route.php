754b0a7f156685808744041414689bf14d1883fd
Pedro Fornaza <pedro.fornaza@gmail.com>
2012-12-07 23:42:52 +0000

    Created tests and class Route

---
+++ src/Kiev/Rest/Route.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Kiev\Rest;
+
+class Route
+{
+	protected $method;
+	protected static $validMethods = array('GET', 'POST', 'PUT', 'DELETE', 'HEAD');
+
+	protected $uri;
+	protected $target;
+
+	public function __construct($method = 'GET', $uri = '/', $target = null)
+	{
+		$this->setMethod($method);
+		$this->setUri($uri);
+
+		if ($target != null) {
+			$this->setTarget($target);
+		}
+	}
+
+	public function setMethod($method)
+	{
+		$method = strtoupper($method);
+		if (!in_array($method, self::$validMethods)) {
+			throw new \InvalidArgumentException('This method is invalid. You should use one of these: '. implode(', ', self::$validMethods) .'.');
+		}
+
+		$this->method = $method;
+	}
+
+	public function getMethod()
+	{
+		return $this->method;
+	}
+
+
+	public function setUri($uri)
+	{
+		$uriParts = parse_url($uri);
+		$uri = isset($uriParts['path']) ? $uriParts['path'] : '/';
+
+		if ($uri !== '/') {
+			$uri = rtrim($uri, '/');
+		}
+
+		$this->uri = $uri;
+	}
+
+	public function getUri()
+	{
+		return $this->uri;
+	}
+
+
+	public function setTarget($target)
+	{
+		if ($target == null ||
+		   (!is_object($target) && !is_string($target))
+		) {
+			throw new \InvalidArgumentException("The target must be a string or an object. The given value: {$target}");
+		}
+
+		$this->target = $target;
+	}
+
+	public function getTarget()
+	{
+		return $this->target;
+	}
+
+
+	public function match($method, $uri)
+	{
+		if ($this->getMethod() == $method &&
+			$this->getUri() == $uri &&
+			$this->getTarget() != null
+		) {
+			return $this->target;
+		}
+
+		return false;
+	}
+
+
+}
+
---
+++ tests/src/Kiev/Rest/RouteTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Kiev\Rest;
+
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+	public function testConstructWithoutParams()
+	{
+		$instance = new Route();
+
+		$this->assertInstanceOf('Kiev\Rest\Route', $instance);
+	}
+
+	public function testConstructWithParams()
+	{
+		$method = 'GET';
+		$uri = '/';
+		$target = 'stdClass';
+		$instance = new Route($method, $uri, $target);
+
+		$this->assertEquals($method, $instance->getMethod());
+		$this->assertEquals($uri, $instance->getUri());
+		$this->assertEquals($target, $instance->getTarget());
+	}
+
+
+	public function testSetterAndGetterWithValidMethod()
+	{
+		$method = 'GET';
+		$instance = new Route();
+
+		$instance->setMethod($method);
+
+		$this->assertEquals($method, $instance->getMethod());
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testSetterAndGetterWithInvalidMethod()
+	{
+		$method = 'BAD_METHOD';
+		$instance = new Route();
+
+		$instance->setMethod($method);
+	}
+
+
+	public function testSetterAndGetterWithValidUri()
+	{
+		$uri = '/';
+		$instance = new Route();
+
+		$instance->setUri($uri);
+
+		$this->assertEquals($uri, $instance->getUri());
+	}
+
+	public function testSetterAndGetterWithDirtyUri()
+	{
+		$dirtyUri = '/test?query=string#test';
+		$cleanUri = '/test';
+		$instance = new Route();
+
+		$instance->setUri($dirtyUri);
+
+		$this->assertEquals($cleanUri, $instance->getUri());
+	}
+
+
+	public function testSetterAndGetterWithStringTarget()
+	{
+		$target = 'stdClass';
+		$instance = new Route();
+
+		$instance->setTarget($target);
+
+		$this->assertEquals($target, $instance->getTarget());
+	}
+
+	public function testSetterAndGetterWithObjectTarget()
+	{
+		$target = new \stdClass();
+		$instance = new Route();
+
+		$instance->setTarget($target);
+
+		$this->assertInstanceOf('stdClass', $instance->getTarget());
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testSetterAndGetterWithInvalidTarget()
+	{
+		$target = '';
+		$instance = new Route();
+
+		$instance->setTarget($target);
+	}
+
+
+	public function testMatchWithRightParams()
+	{
+		$method = 'GET';
+		$uri = '/';
+		$target = 'stdClass';
+
+		$instance = new Route($method, $uri, $target);
+
+		$this->assertEquals($target, $instance->match($method, $uri));
+	}
+
+
+	public function testMatchWithWrongParams()
+	{
+		$method = 'GET';
+		$uri = '/';
+		$target = 'stdClass';
+
+		$instance = new Route($method, $uri, $target);
+
+		$differentMethod = 'POST';
+		$differentUri = '/test';
+
+		$this->assertFalse($instance->match($differentMethod, $differentUri));
+	}
+
+
+}
+
